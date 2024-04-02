a0e658087dc01515829bb2d8eeb22d3050be35e3
MatteusNogueira <matteus.bins@sptech.school>
2024-04-02 18:52:58 -0300

    Tela de cadastro e animações

--- siteV2/js/script.js
+++ siteV2/js/script.js
@@ -1,5 +1,9 @@
 
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 500)
+    header.classList.toggle('rolagem', window.scrollY > 300)
 })        
+
+function mudarTela(tela){
+    window.location.href =`${tela}.html`
+}
