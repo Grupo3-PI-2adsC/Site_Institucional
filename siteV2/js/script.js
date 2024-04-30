1d737b2f296100c10696f390b5db2fcdd11f13ac
MatteusNogueira <matteus.bins@sptech.school>
2024-04-30 19:36:54 -0300

    não lembro

--- siteV2/js/script.js
+++ siteV2/js/script.js
@@ -1,8 +1,8 @@
 
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 300)
-    header.classList.toggle('rolagem2', window.scrollY > 2900)
+    header.classList.toggle('rolagem', window.scrollY > 500)
+    header.classList.toggle('rolagem2', window.scrollY > 3000)
 })        
 
 function mudarTela(tela){
