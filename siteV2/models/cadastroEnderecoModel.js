c01220ee8b60e2db9b9c82d827f9301247b47338
MatteusNogueira <matteus.bins@sptech.school>
2024-05-08 17:55:13 -0300

    subindo modulos

---
+++ siteV2/models/cadastroEnderecoModel.js
@@ -0,0 +1,12 @@
+
+var database = require("../database/config");
+
+function cadastrar(rua, bairro, cidade, estado, cep, numero, complemento, fkEmpresa) {
+  var query = `INSERT INTO endereco (rua, bairro, cidade, estado, cep, numero, complemento, fkEmpresa) VALUES ('${rua}','${bairro}', '${cidade}', '${estado}', '${cep}','${numero}', '${complemento}', '${fkEmpresa}')`;
+console.log(query);
+  return database.executar(query);
+}
+
+module.exports = { 
+    cadastrar
+ };
---
+++ siteV2/models/usuarioModel.js
@@ -0,0 +1,37 @@
+
+var database = require("../database/config")
+
+//LOGIN
+function autenticar(email, senha) {
+    var instrucao = `
+        SELECT idEmpresa, nome, cnpj, email, senha FROM empresa WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+// CADASTRO!
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function cadastrar(nome, cnpj, email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, cnpj, email, senha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO empresa (nome, cnpj, email, senha) VALUES ('${nome}', '${cnpj}', '${email}', '${senha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+function cadastrar2(rua, bairro, cidade, estado, cep, numero, complemento, fkEmpresa) {
+    var instrucao = `INSERT INTO endereco (rua, bairro, cidade, estado, cep, numero, complemento, fkEmpresa) VALUES ('${rua}','${bairro}', '${cidade}', '${estado}', '${cep}','${numero}', '${complemento}', '${fkEmpresa}')`;
+  
+    return database.executar(instrucao);
+  }
+
+module.exports = {
+    autenticar,
+    cadastrar,
+    cadastrar2
+};
