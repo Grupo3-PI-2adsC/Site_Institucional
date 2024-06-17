922e22cc5bbf39b03879c74c3c1e3eb7bf9e158e
gabrieloliveira0505 <gabriel.oliveira165@sptech.school>
2024-06-17 11:05:59 -0300

    dash plotando

---
+++ src/controllers/dashController.js
@@ -0,0 +1,134 @@
+var dashModel = require("../models/dashModel");
+
+function buscarUltimasMedidasCpu(req, res){
+    dashModel.buscarUltimasMedidasCpu(req.body.fkMaquinaServer)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            )
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+}
+function buscarUltimasMedidasRam(req, res){
+    dashModel.buscarUltimasMedidasRam(req.body.fkMaquinaServer)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            )
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+}
+function buscarUltimasMedidasDisco(req, res){
+    dashModel.buscarUltimasMedidasDisco(req.body.fkMaquinaServer)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            )
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+}
+function buscarUltimasMedidasRede(req, res){
+    dashModel.buscarUltimasMedidasRede(req.body.fkMaquinaServer)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            )
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+}
+
+function atualizarMedidasCpu(req, res){
+    dashModel.atualizarMedidasCpu(req.body.fkMaquinaServer)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            )
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+}
+function atualizarMedidasRam(req, res){
+    dashModel.atualizarMedidasRam(req.body.fkMaquinaServer)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            )
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+}
+function atualizarMedidasDisco(req, res){
+    dashModel.atualizarMedidasDisco(req.body.fkMaquinaServer)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            )
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+}
+function atualizarMedidasRede(req, res){
+    dashModel.atualizarMedidasRede(req.body.fkMaquinaServer)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            )
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+}
+
+module.exports = {
+    buscarUltimasMedidasCpu,
+    buscarUltimasMedidasRam,
+    buscarUltimasMedidasDisco,
+    buscarUltimasMedidasRede,
+    atualizarMedidasCpu,
+    atualizarMedidasRam,
+    atualizarMedidasDisco,
+    atualizarMedidasRede
+}
