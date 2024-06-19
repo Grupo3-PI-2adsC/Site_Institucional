c9b5213aaa72ccf98ced0df1ff541c191063a6c3
ketelynMedina <ketelyn.martins@sptech.school>
2024-06-19 16:54:25 -0300

    Ajustando

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,13 @@
 
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
+    let nav = document.querySelector('#nav_logo')
+    let login = document.querySelector('#btn-login')
+    
     header.classList.toggle('rolagem', window.scrollY > 300)
-    header.classList.toggle('rolagem2', window.scrollY > 2900)
+    header.classList.toggle('rolagem2', window.scrollY > 3500)
+    nav.classList.toggle('rolagem2', window.scrollY > 3500)
+    login.classList.toggle('rolagem2', window.screenY > 3500)
 })        
 
 function mudarTela(tela){
